8a06f76c565199aea868e547d025f1e6d923eac8
bowwow2115 <bowwow2115@gmail.com>
2022-08-14 10:30:08 +0900

    Add realationship ingredeint with recipe

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Created by jt on 6/13/17.
@@ -27,6 +28,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
